f702d54ed855391855f5d6ece20cc36252018aa1
SYZBA1 <samyos9090@gmail.com>
2024-10-17 10:20:40 +0000

    update

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,7 @@
--- Script to create triggers
--- Update table when a new order is added
-delimiter //
-CREATE TRIGGER update_inventory
-AFTER INSERT ON orders
-FOR EACH ROW
-UPDATE items
-SET quantity = quantity - 1
-WHERE name = NEW.item_name;
+-- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
+-- Context: Updating multiple tables for one action from your application can generate issue: network disconnection,
+-- crash, etc… to keep your data in a good shape, let MySQL do it for you!
+
+CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
